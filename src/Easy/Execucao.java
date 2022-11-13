fce8324bf66db6831368ef119e8c592a6a2e15ef
aline-ayrao <alineayrao@gmail.com>
2022-11-12 21:47:52 -0300

    Create Execucao.java

---
+++ src/Easy/Execucao.java
@@ -0,0 +1,12 @@
+package Easy;
+
+public class Execucao {
+    
+    public static void main(String[] args) {
+        Exercicio02_easy exercicio02_easy = new Exercicio02_easy();
+        String palavra = exercicio02_easy.retornaPalavra("Aline Ayrão");
+        System.out.println(palavra);
+
+        //System.out.println(exercicio02_easy.retornaPalavra("Aline Ayrão"));
+    }
+}
