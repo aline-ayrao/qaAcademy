464d290c86abac50f87f1c03a8e2effd0b18d655
aline-ayrao <alineayrao@gmail.com>
2022-11-12 21:50:16 -0300

    Update Exercicio02_easy.java

--- src/Easy/Exercicio02_easy.java
+++ src/Easy/Exercicio02_easy.java
@@ -1,11 +1,9 @@
 package Easy;
-import javax.swing.JOptionPane;
 
 public class Exercicio02_easy { // inicio classe exercicio
 
-    public static void main(String[] args) {
-        String palavraDigitada;
-        palavraDigitada = JOptionPane.showInputDialog("Digite uma palavra!");
-        System.out.println("O Usuario digitou: "+palavraDigitada);
+    public String retornaPalavra(String palavra) {
+       
+        return "O Usuario digitou: "+palavra;
     } 
 }// fim exercicio
