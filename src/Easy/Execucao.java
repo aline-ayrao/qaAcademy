f623458f33dea1a8c1b8dbc048550ab310b807d4
aline-ayrao <alineayrao@gmail.com>
2022-11-13 00:13:00 -0300

    Alteração Exercicio06

--- src/Easy/Execucao.java
+++ src/Easy/Execucao.java
@@ -22,20 +22,32 @@ public class Execucao {
         // Exercicio04_easy exercicio04_easy = new Exercicio04_easy();
         // System.out.println(exercicio04_easy.calcularDobro(numeroDigitado));
 
-        String dig1 = JOptionPane.showInputDialog("Digite o primeiro numero");
-        String dig2 = JOptionPane.showInputDialog("Digite o segungo numero");
-        String dig3 = JOptionPane.showInputDialog("Digite o terceiro numero");
+        // String dig1 = JOptionPane.showInputDialog("Digite o primeiro numero");
+        // String dig2 = JOptionPane.showInputDialog("Digite o segungo numero");
+        // String dig3 = JOptionPane.showInputDialog("Digite o terceiro numero");
 
-        int num1 = Integer.parseInt(dig1);
-        int num2 = Integer.parseInt(dig2);
-        int num3 = Integer.parseInt(dig3);
+        // int num1 = Integer.parseInt(dig1);
+        // int num2 = Integer.parseInt(dig2);
+        // int num3 = Integer.parseInt(dig3);
 
-        Exercicio05_easy exercicio05_easy = new Exercicio05_easy();
-        System.out.println("A Soma é: " + exercicio05_easy.soma(num1, num2, num3));
-        System.out.println("A subtração é: " + exercicio05_easy.subtracao(num1, num2, num3));
-        System.out.println("A multiplicação é: " + exercicio05_easy.multiplicacao(num1, num2, num3));
-        int soma = exercicio05_easy.soma(num1, num2, num3);
-        System.out.println("A média é: " + exercicio05_easy.media(soma));
+        // Exercicio05_easy exercicio05_easy = new Exercicio05_easy();
+        // System.out.println("A Soma é: " + exercicio05_easy.soma(num1, num2, num3));
+        // System.out.println("A subtração é: " + exercicio05_easy.subtracao(num1, num2,
+        // num3));
+        // System.out.println("A multiplicação é: " +
+        // exercicio05_easy.multiplicacao(num1, num2, num3));
+        // int soma = exercicio05_easy.soma(num1, num2, num3);
+        // System.out.println("A média é: " + exercicio05_easy.media(soma));
+
+        String nota1Stg, nota2Stg;
+        nota1Stg = JOptionPane.showInputDialog("Por Favor Digite a primeira nota");
+        nota2Stg = JOptionPane.showInputDialog("Por Favor Digite a segunda nota");
+
+        double nota1 = Double.parseDouble(nota1Stg);
+        double nota2 = Double.parseDouble(nota2Stg);
+
+        Exercicio06_easy exercicio06_easy = new Exercicio06_easy();
+        System.out.println(exercicio06_easy.verificaAprovacao(nota1, nota2));
 
     }
 }
--- src/Easy/Exercicio06_easy.java
+++ src/Easy/Exercicio06_easy.java
@@ -1,28 +1,20 @@
 package Easy;
-import javax.swing.JOptionPane;
 
 public class Exercicio06_easy {
-    public static void main(String[] args) {
-        double nota1, nota2, resultado;
-        String nota1Stg, nota2Stg;
-        nota1Stg = JOptionPane.showInputDialog("Por Favor Digite a primeira nota");
-        nota2Stg = JOptionPane.showInputDialog("Por Favor Digite a segunda nota");
-
-        nota1 = Integer.parseInt(nota1Stg);
-        nota2 = Integer.parseInt(nota2Stg);
+    public String verificaAprovacao(double nota1, double nota2) {
+        double resultado;
         resultado = (nota1 + nota2)/2;
-        JOptionPane.showMessageDialog(null,"Seu resultado foi:" + resultado);
-
         if(resultado > 5){
-            JOptionPane.showMessageDialog(null,"Aprovado");
+        return "Aprovado";
         } // fim se
 
         if(resultado < 5){
-            JOptionPane.showMessageDialog(null,"Reprovado");
+            return"Reprovado";
         }// fim se
 
         if (resultado == 5){
-            JOptionPane.showMessageDialog(null,"Exame");
-        }//fim se     
+            return"Exame";
+        }//fim se   
+        return "Nota não calculada";  
     } // fim metodo main
 } //fim da classe
