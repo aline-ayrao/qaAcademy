fcf67baec0b093220f8bd20619e6d79939edeb86
aline-ayrao <alineayrao@gmail.com>
2022-11-13 17:50:47 -0300

    Alteração exercicio06 Medium

--- src/Medium/Execucao.java
+++ src/Medium/Execucao.java
@@ -8,14 +8,18 @@ public class Execucao {
         // exercicio01_medium.exibeParOuImpar();
 
         // Exercicio02_medium exercicio02_medium = new Exercicio02_medium();
-        // double valorInvestimento = Double.parseDouble(JOptionPane.showInputDialog("Digite um valor:"));
+        // double valorInvestimento =
+        // Double.parseDouble(JOptionPane.showInputDialog("Digite um valor:"));
         // double valorJuros, valorTotal;
-        // valorJuros = exercicio02_medium.calculaInvestimentoJurosCompostos(valorInvestimento);
-        // valorTotal = exercicio02_medium.calculaValorTotalInvestimento(valorInvestimento,valorJuros);
-        
+        // valorJuros =
+        // exercicio02_medium.calculaInvestimentoJurosCompostos(valorInvestimento);
+        // valorTotal =
+        // exercicio02_medium.calculaValorTotalInvestimento(valorInvestimento,valorJuros);
+
         // System.out.println("O valor do investimento é: R$"+ valorInvestimento);
         // System.out.println("O valor do juros é: R$"+(valorJuros));
-        // System.out.println("O valor total do investimento: R$"+ (valorJuros + valorInvestimento));
+        // System.out.println("O valor total do investimento: R$"+ (valorJuros +
+        // valorInvestimento));
 
         // Exercicio03_medium exercicio03_medium = new Exercicio03_medium();
         // exercicio03_medium.somaNumerosAcumulados();
@@ -23,9 +27,13 @@ public class Execucao {
         // Exercicio04_medium exercicio04_medium = new Exercicio04_medium();
         // exercicio04_medium.multiplicacaoNumerosAteMil();
 
-        Exercicio05_medium exercicio05_medium = new Exercicio05_medium();
-        int menorNumero = exercicio05_medium.exibeMenorNumero();
-                System.out.println("O numero: " + menorNumero + " é o menor.");
+        // Exercicio05_medium exercicio05_medium = new Exercicio05_medium();
+        // int menorNumero = exercicio05_medium.exibeMenorNumero();
+        // System.out.println("O numero: " + menorNumero + " é o menor.");
+
+        Exercicio06_medium exercicio06_medium = new Exercicio06_medium();
+        int fatorial = Integer.parseInt(JOptionPane.showInputDialog("Digite um numero:"));
+        System.out.println("O fatorial do numero digitado é:" + (exercicio06_medium.calculaFatorial(fatorial)));
     }
 
 }
--- src/Medium/Exercicio06_medium.java
+++ src/Medium/Exercicio06_medium.java
@@ -1,16 +1,14 @@
 package Medium;
 
-import javax.swing.JOptionPane;
-
 public class Exercicio06_medium {
-    public static void main(String[] args) {
-        int fatorial, i;
-        fatorial = Integer.parseInt(JOptionPane.showInputDialog("Digite um numero:"));
+    public int calculaFatorial(int fatorial) {
+        int i;
+       
         i=fatorial;
         while (i>1) {
             fatorial = fatorial*(i-1);
             i--;            
         }
-        System.out.println("O fatorial do numero digitado é:"+fatorial);
+        return fatorial;
     }
 }
