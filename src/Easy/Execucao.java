72f6beee215568659a99ea8bd793754930b500b7
aline-ayrao <alineayrao@gmail.com>
2022-11-12 23:00:41 -0300

    Alteração exercicio 3

--- src/Easy/Execucao.java
+++ src/Easy/Execucao.java
@@ -1,12 +1,18 @@
 package Easy;
 
+import javax.swing.JOptionPane;
+
 public class Execucao {
     
     public static void main(String[] args) {
-        Exercicio02_easy exercicio02_easy = new Exercicio02_easy();
-        String palavra = exercicio02_easy.retornaPalavra("Aline Ayrão");
-        System.out.println(palavra);
+        // Exercicio02_easy exercicio02_easy = new Exercicio02_easy();
+        // String palavra = exercicio02_easy.retornaPalavra("Aline Ayrão");
+        // System.out.println(palavra);
 
         //System.out.println(exercicio02_easy.retornaPalavra("Aline Ayrão"));
+        String primeiroValor = JOptionPane.showInputDialog("Digite um valor");
+        String segundoValor = JOptionPane.showInputDialog("Digite outro valor");
+        Exercicio03_easy exercicio03_easy = new Exercicio03_easy();
+        System.out.println(exercicio03_easy.retornaValorTrocado(primeiroValor, segundoValor));
     }
 }
--- src/Easy/Exercicio03_easy.java
+++ src/Easy/Exercicio03_easy.java
@@ -1,15 +1,11 @@
 package Easy;
-import javax.swing.JOptionPane;
 
 public class Exercicio03_easy {
-    public static void main(String[] args) {
-
-    String primeiroValor, segundoValor;
-    primeiroValor = JOptionPane.showInputDialog("Digite um valor");
-    segundoValor = JOptionPane.showInputDialog("Digite outro valor");
-    System.out.println("O primeiro valor digitado foi: "+primeiroValor);
-    System.out.println("O primeiro valor digitado foi: "+segundoValor);   
+   static  String primeiroValor;
+    static String segundoValor;
+    
+    public String retornaValorTrocado(String primeiroValor, String segundoValor){
+ 
+    return "O primeiro valor digitado foi: "+segundoValor + "\n" + "O segundo valor digitado foi: "+primeiroValor;   
     }
-    
-    
 }
